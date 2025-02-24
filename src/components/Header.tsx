f92ebe854053e9e4b0c1fce803b0cb4561f0381b
TAT610 <trananhtuyet0610@gmail.com>
2025-02-24 13:05:46 +0700

    header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,32 +2,34 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header className="fixed top-0 left-0 w-full bg-white shadow-md z-50 flex justify-between items-center py-4 px-8 border-b">
-      <div className="flex items-center space-x-2">
+    <header className="fixed top-0 left-0 w-full bg-white shadow-md z-50 flex justify-between items-center py-4 px-8 ">
+      <div className="flex w-1/4 items-center space-x-2">
         <img src="src/assets/logo.png" alt="MarriTalk Logo" className="h-6" />
       </div>
-      <nav className="flex space-x-6 font-medium text-teal-700 ">
+      <nav className="flex-1 flex justify-center space-x-6 font-semibold text-[#325343]  ">
         <a href="/" className="hover:underline">
-          <p className="text-teal-700">Home</p>
+          <p className="text-[#325343] text-base">Home</p>
         </a>
         <a href="#" className="hover:underline">
-          <p className="text-teal-700"> New & Blog</p>
+          <p className="text-[#325343]   text-base"> New & Blog</p>
         </a>
         <a href="/counseling" className="hover:underline">
-          <p className="text-teal-700">Counseling</p>
+          <p className="text-[#325343]  text-base">Counseling</p>
         </a>
         <a href="/quizzes" className="hover:underline">
-          <p className="text-teal-700"> Quizzes</p>
+          <p className="text-[#325343]  text-base"> Quizzes</p>
         </a>
         <a href="#" className="hover:underline">
-          <p className="text-teal-700"> Contact</p>
+          <p className="text-[#325343]   text-base"> Contact</p>
         </a>
       </nav>
-      <Link to="/login">
-        <button className="border border-teal-700 text-teal-700 px-4 py-1 rounded-full hover:bg-teal-700 hover:text-white transition">
-          Login
-        </button>
-      </Link>
+      <div className="w-1/4 flex justify-end">
+        <Link to="/login">
+          <button className="border-2 border-[#325343]  text-[#325343] font-semibold cursor-pointer px-5 py-1 rounded-full hover:bg-teal-700 hover:text-white transition">
+            Login
+          </button>
+        </Link>
+      </div>
     </header>
   );
 }
--- src/components/Header2.tsx
+++ src/components/Header2.tsx
@@ -1,10 +1,10 @@
 export default function Header2() {
   return (
     <header className="fixed top-0 left-0 w-full bg-[#325343] z-50 flex justify-between items-center py-4 px-8 ">
-      <div className="flex items-center space-x-2">
+      <div className="flex w-1/4 items-center space-x-2">
         <img src="src/assets/logo1.png" alt="MarriTalk Logo" className="h-10" />
       </div>
-      <nav className="flex space-x-6 font-semibold ">
+      <nav className="flex-1 flex justify-center space-x-6 font-semibold ">
         <a href="/" className="hover:underline">
           <p className="text-white text-base">Home</p>
         </a>
@@ -21,9 +21,11 @@ export default function Header2() {
           <p className="text-white text-base"> Contact</p>
         </a>
       </nav>
-      <button className="bg-white border font-bold border-white text-[#325343] px-5 py-2 rounded-full  cursor-pointer transition duration-500 opacity-100 hover:opacity-60">
-        Sign up
-      </button>
+      <div className="w-1/4 flex justify-end">
+        <button className="bg-white border font-bold border-white text-[#325343] px-5 py-2 rounded-full  cursor-pointer transition duration-500 opacity-100 hover:opacity-60">
+          Sign up
+        </button>
+      </div>
     </header>
   );
 }
